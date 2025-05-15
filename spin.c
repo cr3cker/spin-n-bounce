ddc8e30b5583a4de7ef357cc4b6be605246a1779
Vladislav <vladislavvv1606@gmail.com>
2025-05-15 20:46:41 +0200

    fixes and elastic collisions

--- spin.c
+++ spin.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include "raylib.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
@@ -8,6 +9,7 @@
 #define END 330
 #define SPEED_LIMIT 100.0f
 #define SIZE 800
+#define SUBTICK 5
 
 const Vector2 gravity = { 0.0f, 150.0f };
 const Vector2 center = { SIZE / 2.0f, SIZE / 2.0f };
@@ -28,6 +30,7 @@ typedef struct {
     float mass;
     Color color;
     bool bounced;
+    float spawn_cooldown;
 } Ball;
 
 Vector2 vector2mul(Vector2 vec1, Vector2 vec2) {
@@ -153,6 +156,77 @@ Ring *remove_ring(Ring *rings_arr, int *rings_n) {
     return rings_arr;
 }
 
+// this crazy stuff is taken from stackoverflow, after reading a proof, I cried and left the article in agony
+void elastic_collision(Ball *b1, Ball *b2) {
+    Vector2 x1 = b1->pos;
+    Vector2 x2 = b2->pos;
+    Vector2 v1 = b1->vel;
+    Vector2 v2 = b2->vel;
+    float m1 = b1->mass;
+    float m2 = b2->mass;
+
+    Vector2 delta_pos = vector2subtract(x1, x2);
+    Vector2 delta_vel = vector2subtract(v1, v2);
+    float dist_squared = vector2dot(delta_pos, delta_pos);
+
+    float dot_product = vector2dot(delta_vel, delta_pos);
+
+    float factor1 = (2.0f * m2 / (m1 + m2)) * (dot_product / dist_squared);
+    Vector2 correction1 = vector2scale(delta_pos, factor1);
+    b1->vel = vector2subtract(v1, correction1);
+
+    delta_pos = vector2subtract(x2, x1);
+    delta_vel = vector2subtract(v2, v1);
+    dot_product = vector2dot(delta_vel, delta_pos);
+    float factor2 = (2.0f * m1 / (m1 + m2)) * (dot_product / dist_squared);
+    Vector2 correction2 = vector2scale(delta_pos, factor2);
+    b2->vel = vector2subtract(v2, correction2);
+}
+
+void decrease_cooldown(Ball *balls_arr, int balls_n, float dt) {
+    for (int i = 0; i < balls_n; i++) {
+        if (balls_arr[i].spawn_cooldown > 0) {
+            balls_arr[i].spawn_cooldown -= dt;
+        }
+    }
+}
+
+void add_ball(int *balls_n, int *rings_n, Ball **balls_arr, Ring **rings_arr) {
+    for (int j = 0; j < *balls_n; j++) {
+        int i = 0;
+        while (i < *rings_n) {
+            if (distance((*balls_arr)[j].pos, center) > (*rings_arr)[i].inner_r - (*balls_arr)[j].radius) {
+                (*balls_n)++;
+                *balls_arr = realloc(*balls_arr, (*balls_n) * sizeof(Ball));
+
+                Ball new_ball = (*balls_arr)[j];
+                new_ball.spawn_cooldown = 1.0f;
+                new_ball.pos.x += GetRandomValue(-10, 10);
+                new_ball.pos.y += GetRandomValue(-10, 10);
+
+                (*balls_arr)[*balls_n - 1] = new_ball;
+                *rings_arr = remove_ring(*rings_arr, rings_n);
+                (*balls_arr)[j].radius *= 1.2f;
+                (*balls_arr)[j].vel = vector2scale((*balls_arr)[j].vel, 1.1f);
+                break;
+            } else {
+                i++;
+            }
+        }
+    }
+}
+
+void handle_ball_collisions(Ball *balls, int balls_n) {
+    for (int i = 0; i < balls_n; i++) {
+        for (int j = i + 1; j < balls_n; j++) {
+            if (balls[i].spawn_cooldown <= 0 && balls[j].spawn_cooldown <= 0 &&
+                CheckCollisionCircles(balls[i].pos, balls[i].radius, balls[j].pos, balls[j].radius)) {
+                elastic_collision(&balls[i], &balls[j]);
+            }
+        }
+    }
+}
+
 int main() {
     InitWindow(800, 800, "Centrifuuuga");
     Vector2 center = { SIZE / 2.f, SIZE / 2.f };
@@ -161,7 +235,7 @@ int main() {
     Color ball_color = { 0, 10, 20, 255 };
     Ring *rings_arr = generate_rings(rings_n);
     Ball *balls_arr = (Ball *)malloc(balls_n * sizeof(Ball));
-    Ball ball = { center, (Vector2){ 10.0f, 10.0f }, 8, 10, ball_color, false };
+    Ball ball = { center, (Vector2){ 10.0f, 10.0f }, 8, 10, ball_color, false, 1.0f };
     balls_arr[0] = ball;
 
     while (!WindowShouldClose()) {
@@ -169,25 +243,14 @@ int main() {
         ClearBackground(BLACK);
         BeginDrawing();
 
+        decrease_cooldown(balls_arr, balls_n, dt);
+
+        handle_ball_collisions(balls_arr, balls_n);
+        add_ball(&balls_n, &rings_n, &balls_arr, &rings_arr);
+
         for (int i = 0; i < balls_n; i++) {
             draw_ball(balls_arr[i]);
             update_ball(&balls_arr[i], rings_arr, rings_n, dt);
-        }
-
-        for (int j = 0; j < balls_n; j++) {
-            int i = 0;
-            while (i < rings_n) {
-                if (distance(balls_arr[j].pos, center) > rings_arr[i].inner_r - balls_arr[j].radius) {
-                    balls_n++;
-                    balls_arr = realloc(balls_arr, balls_n * sizeof(Ball));
-                    balls_arr[balls_n - 1] = balls_arr[j];
-                    rings_arr = remove_ring(rings_arr, &rings_n);
-                    balls_arr[j].radius *= 1.2f;
-                    balls_arr[j].vel = vector2scale(balls_arr[j].vel, 1.1f);
-                } else {
-                    i++;
-                }
-            }
         }
 
         for (int i = 0; i < rings_n; i++) {
